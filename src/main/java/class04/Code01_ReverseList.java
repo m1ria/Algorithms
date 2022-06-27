1b6fc43f346859943f338b3e6dd0f1fe88bb4ea4
m1ria <chzaaow@gmail.com>
2022-06-27 23:22:53 +0800

    add: List

---
+++ src/main/java/class04/Code01_ReverseList.java
@@ -0,0 +1,55 @@
+package class04;
+
+/**
+ * @className: Code01_ReverseList
+ * @description: 链表翻转
+ * @author: m1ria
+ * @date: 2022/6/27 16:57
+ * @version: 1.0
+ */
+public class Code01_ReverseList {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            value = data;
+        }
+    }
+
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int data) {
+            value = data;
+        }
+    }
+
+    public static Node reverseLinkedList(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public static DoubleNode reverseDoubleList(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.last = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
