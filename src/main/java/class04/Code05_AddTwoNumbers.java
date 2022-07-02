b52a7baa0facf895820609b842c989a090e23af5
m1ria <chzaaow@gmail.com>
2022-07-02 23:17:54 +0800

    add: list

---
+++ src/main/java/class04/Code05_AddTwoNumbers.java
@@ -0,0 +1,67 @@
+package class04;
+
+/**
+ * @className: Code05_AddTwoNumbers
+ * @description: 两个有序列表 相加
+ * @author: m1ria
+ * @date: 2022/7/2 22:32
+ * @version: 1.0
+ */
+public class Code05_AddTwoNumbers {
+
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static int listLength(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+
+    public static ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+
+        int len1 = listLength(head1);
+        int len2 = listLength(head2);
+
+        ListNode l = len1 >= len2 ? head1 : head2;
+        ListNode s = l == head1 ? head2 : head1;
+        ListNode curL = l;
+        ListNode curS = s;
+        ListNode last = curL;
+        int carry = 0;
+        int curNum = 0;
+        while (curS != null) {
+            curNum = curL.val + curS.val + carry;
+            curL.val = (curNum % 10);
+            carry = curNum / 10;
+            last = curL;
+            curL = curL.next;
+            curS = curS.next;
+        }
+        while (curL != null) {
+            curNum = curL.val + carry;
+            curL.val = (curNum % 10);
+            carry = curNum / 10;
+            last = curL;
+            curL = curL.next;
+        }
+        if (carry != 0) {
+            last.next = new ListNode(1);
+        }
+        return l;
+    }
+}
