89f189dbb9987530b9242bac7bdf6725811d644d
m1ria <chzaaow@gmail.com>
2022-07-21 03:46:33 +0800

    add: quick

---
+++ src/main/java/class03/Code05_QuickSort.java
@@ -0,0 +1,58 @@
+package class03;
+
+import java.util.Arrays;
+
+/**
+ * @className: Code05_QuickSort
+ * @description: TODO
+ * @author: m1ria
+ * @date: 2022/7/20 5:16
+ * @version: 1.0
+ */
+public class Code05_QuickSort {
+
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
+            int[] p = partition(arr, l, r);
+            quickSort(arr, l, p[0] - 1);
+            quickSort(arr, p[1] + 1, r);
+        }
+    }
+
+    public static int[] partition(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while (l < more) {
+            if (arr[l] < arr[r]) {
+                swap(arr, ++less, l++);
+            } else if (arr[l] > arr[r]) {
+                swap(arr, --more, l);
+            } else {
+                l++;
+            }
+        }
+        swap(arr, more, r);
+        return new int[]{less + 1, more};
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 21, 3, 9, 4};
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
